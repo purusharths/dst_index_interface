90394a497990fa2754d6987764ff9bd4a72c92aa
Purusharth <saxena.puru005@gmail.com>
2018-03-13 01:17:29 +0530

    Single day plotting

--- plotting.py
+++ plotting.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+from datetime import datetime
 class Plotting(object):
 	"""docstring for Plotting"""
 	def __init__(self, **kwargs):
@@ -7,15 +7,26 @@ class Plotting(object):
 		self.month = kwargs.get('month', '')
 		self.day = kwargs.get('day', '')
 
+	def get_formatted_date(self, **kwargs):
+		year = kwargs.get('year')
+		month = kwargs.get('month')
+		day = kwargs.get('day', '')
+		if day:
+			dt_obj = datetime.strptime("{}/{}/{}".format(year, month, day), "%Y/%M/%d")
+			return dt_obj.strftime("%B %d, %Y (%A)")
+		else:
+			dt_obj = datetime.strptime("{}/{}".format(year, month), "%Y/%M")
+			return dt_obj.strftime("%B - %Y")
+
 	def normal_plot(self, value, day=True):
 		if not day:
 			pass
 		else:
-			plt.style.use('ggplot')
+			plt.style.use('seaborn')
 			plt.plot(value,'o-')
 			plt.xticks(range(0,24))
-			plt.xlabel("Hours Through The Day")
+			plt.xlabel("Hours throughout the day")
 			plt.ylabel("Values")
-			title = "Dst Index for {}/{}/{}".format(self.year, self.month,self.day)
+			title = self.get_formatted_date(year=self.year, month=self.month, day=self.day)
 			plt.title(title)
-			plt.show()		
+			plt.show()
--- test.py
+++ test.py
@@ -9,12 +9,11 @@ import plotting
 def get_day(year, month, day, rtr=False):
         response = get_data.get_value_from_database(year, month, day)
         if response:
+                if rtr:
+                    return json.loads(response[0][1])
                 print("\n YEAR: {} | MONTH: {} | DAY: {}".format(year, month, day))
                 print(response[0][1])
-                if rtr:
-                        return json.loads(response[0][1])
-                else:
-                        return 1
+                return 1
         else:
                 get_data.main(year,month,display=False)
                 get_day(year, month, day)
@@ -27,18 +26,22 @@ def command_line_arguments(year, month, day, plot_day):
             print("Invalid Date for given month and year. Exiting...\n")
             exit()
         val = get_day(year, month, day)
-    elif not day and month and year:
-        print(month, year, day)
-        get_data.main(year, month)
+
+        if plot_day:
+            try:
+                plot_points = get_day(year, month, day, rtr=True); val=False
+                pl = plotting.Plotting(year=year, month=month, day=day)
+                pl.normal_plot(plot_points)
+                return "plotted"
+            except IndexError:
+                pass
+
+    elif day == 'None':
+        if month and year:
+            print("\n YEAR: {} | MONTH: {} | \n".format(year, month))
+            get_data.main(year, month)
     
-    if plot_day:
-        try:
-            plot_points = get_day(year, month, day, rtr=True); val=False
-            pl = plotting.Plotting(year=year, month=month, day=day)
-            pl.normal_plot(plot_points)
-            return "plotted"
-        except IndexError:
-            pass
+
 
 if __name__ == '__main__':
 
@@ -52,11 +55,12 @@ if __name__ == '__main__':
     
     n = lambda x: ('0'+str(x)) if len(str(x)) == 1 else str(x)
 
-    parser = argparse.ArgumentParser('My program')
-    parser.add_argument('-y', '--year', type=int, choices=range(1957,2014))
-    parser.add_argument('-m', '--month', type=int, choices=range(1,13))
-    parser.add_argument('-d', '--day', type=int, choices=range(1,32))
-    parser.add_argument('--plot-day', type=str2bool, nargs='?', const=True)
+    parser = argparse.ArgumentParser('DST Index Command Line Interface')
+    parser.add_argument('-y', '--year', type=int, choices=range(1957,2014), help="Year Value (Should be between 1950 and 2013)")
+    parser.add_argument('-m', '--month', type=int, choices=range(1,13), help="Month")
+    parser.add_argument('-d', '--day', type=int, choices=range(1,32), help="Day (Optional)")
+    parser.add_argument('--plot-day', type=str2bool, nargs='?', const=True, help="For getting the plot for a Single Day")
+    parser.add_argument('--plot-month', type=str2bool, nargs='?', const=True, help="For getting the plot for an Entire Month")
     args = vars(parser.parse_args())
     
     command_line_arguments(str(args['year']), n(args['month']), n(args['day']), args['plot_day'])
