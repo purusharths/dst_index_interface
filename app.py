69014aa8fc8c4798946e89306bf426c21e6f941e
Purusharth <saxena.puru005@gmail.com>
2018-03-14 01:08:12 +0530

    Fixed templates/error.html

--- app.py
+++ app.py
@@ -14,46 +14,48 @@ def index():
     year = request.args.get('year')
     month = request.args.get('month')
     if year and month:
-        if int(year) > 1957 and int(year) < 2014:# and int(month)>1 and int(month)<12:
-            n = lambda x: ('0'+str(x)) if len(str(x)) == 1 else str(x)
-            month = n(month)
-            day = request.args.get('day','')
-            plot_options = request.args.get('plotOptions', '')
-            # if day value is given
-            if day:
-                day = n(day)
-                limit = monthrange(int(year), int(month))[1]
-                if int(day) > limit:
-                    return render_template("error.html", error_message=error_message)
-                contents = main.get_day(year, month, day, rtr=True)
-                if not contents:
+        if int(year) > 1957 and int(year) < 2014:# and 
+            if int(month)>1 and int(month)<13:
+                n = lambda x: ('0'+str(x)) if len(str(x)) == 1 else str(x)
+                month = n(month)
+                day = request.args.get('day','')
+                plot_options = request.args.get('plotOptions', '')
+                # if day value is given
+                if day:
+                    day = n(day)
+                    limit = monthrange(int(year), int(month))[1]
+                    if int(day) > limit:
+                        return render_template("error.html", error_message="Invalid day specified for the given month/year. Please Try again")
                     contents = main.get_day(year, month, day, rtr=True)
-                #getting date
-                pl = plotting.Plotting(year=year, month=month, day=day)
-                date = pl.get_formatted_date(year=year, month=month, day=day)
-                if plot_options:
-                    filename = pl.normal_plot(contents, savefigure=True)
-                    return render_template('index.html', day_contents=contents, date=date, filename=filename)
+                    if not contents:
+                        contents = main.get_day(year, month, day, rtr=True)
+                    #getting date
+                    pl = plotting.Plotting(year=year, month=month, day=day)
+                    date = pl.get_formatted_date(year=year, month=month, day=day)
+                    if plot_options:
+                        filename = pl.normal_plot(contents, savefigure=True)
+                        return render_template('index.html', day_contents=contents, date=date, filename=filename)
 
-                return render_template('index.html', day_contents=contents, date=date)
-            # if day value is not given
-            else:
-                contents = main.get_month(year, month, get_raw_data=True)
-                if not contents:
+                    return render_template('index.html', day_contents=contents, date=date)
+                # if day value is not given
+                else:
                     contents = main.get_month(year, month, get_raw_data=True)
-                contents = ancillary.convert_to_dict(string_list=contents)
-                # getting date
-                plot_month = plotting.Plotting()
-                date = plot_month.get_formatted_date(year=year, month=month)
-                if plot_options:
-                    avg_val = main.get_month(year, month, get_raw_data=False)
-                    plot_month.year = year; plot_month.month = month
-                    filename = plot_month.normal_plot(avg_val,  day=False, savefigure=True)
-                    return render_template('index.html', month_contents=contents, date=date, filename=filename)
-                return render_template('index.html', month_contents=contents, date=date)
-            
+                    if not contents:
+                        contents = main.get_month(year, month, get_raw_data=True)
+                    contents = ancillary.convert_to_dict(string_list=contents)
+                    # getting date
+                    plot_month = plotting.Plotting()
+                    date = plot_month.get_formatted_date(year=year, month=month)
+                    if plot_options:
+                        avg_val = main.get_month(year, month, get_raw_data=False)
+                        plot_month.year = year; plot_month.month = month
+                        filename = plot_month.normal_plot(avg_val,  day=False, savefigure=True)
+                        return render_template('index.html', month_contents=contents, date=date, filename=filename)
+                    return render_template('index.html', month_contents=contents, date=date)
+            else:
+                return render_template("error.html", error_message="Invalid Month. Please try again.")
         else:
-            return render_template("error_year.html")
+            return render_template("error.html", error_message="Year outside the specified Scope. Please try again.")
     else:
         return render_template('index.html')
     return render_template('index.html')
